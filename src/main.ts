0dd165205c7a69b6462a361886ede7dbac23ae29
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2025-01-31 16:42:27 +0200

    chore: setup swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+      .setTitle('Estate connect api')
+      .setDescription('The Estate connect api description')
+      .setVersion('1.0')
+      .addTag('Estate connect')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, documentFactory);
+
+    await app.listen(3000);
+  } catch (error) {
+    console.error('error', error);
+  }
 }
 bootstrap();
